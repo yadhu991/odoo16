9025a3539358265bfc62bd31812f42be1a34aef0
yadhu991 <yadu991@gmail.com>
2023-06-20 17:05:26 +0530

    my next commit

---
+++ payment_paytm/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Payment Provider : Paytm',
+    'version': '16.0.1.0.0',
+    'category': 'Payment provider/Paytm',
+    'description': "Module for integrating Paytm provider t oodoo payments.",
+    'summary': 'Payments',
+    'sequence': 5,
+    'depends': ['base', 'payment'],
+    'license': 'LGPL-3',
+    'data': [
+        'data/paytm_payment_record.xml',
+
+    ],
+    'images': [
+        'static/description/icon.png'
+    ],
+    'application': False,
+    'installable': True,
+    'auto_install': True,
+
+}
